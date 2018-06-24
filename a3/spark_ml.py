5f544f2373caa50985a1ddc442f17d05e8ce9825
Zongming Liu <zongmingxliu@gmail.com>
2018-06-24 19:45:35 -0300

    almost finished

--- a3/spark_ml.py
+++ a3/spark_ml.py
@@ -1,84 +1,89 @@
 import findspark
 findspark.init()
 
-from pyspark.sql import SQLContext
-from pyspark import SparkContext
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
-from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
+from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer, ChiSqSelector
 from pyspark.ml.classification import LogisticRegression
 
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler, IndexToString
 
-from pyspark.ml.evaluation import BinaryClassificationEvaluator,MulticlassClassificationEvaluator
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-#from pyspark.ml import PipelineModel
+spark = SparkSession \
+    .builder \
+    .appName("5408-A3") \
+    .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+    .getOrCreate()
 
-sc =SparkContext()
-sqlContext = SQLContext(sc)
-data = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('train.csv')
+pipeline_model = PipelineModel.load("logreg.model")
 
-drop_list = ['ItemID']
-data = data.select([column for column in data.columns if column not in drop_list])
-data.show(5)
+# https://stackoverflow.com/a/44558280/8263143
+schema = StructType([
+    StructField("polarity", StringType(), True),
+    StructField("id", StringType(), True),
+    StructField("date", StringType(), True),
+    StructField("query", StringType(), True),
+    StructField("user", StringType(), True),
+    StructField("text", StringType(), True)
+    ])
 
-data.printSchema()
+df = spark.read.csv("testdata.manual.2009.06.14.csv", header=False, schema=schema)
 
-data.groupBy("Sentiment") \
-    .count() \
-    .orderBy(col("count").desc()) \
-    .show()
+drop_list = ["id", "date", "query", "user"]
+df = df.select([column for column in df.columns if column not in drop_list])
+df.show(5)
 
-data.groupBy("SentimentText") \
-    .count() \
-    .orderBy(col("count").desc()) \
-    .show()
+df.printSchema()
+
+df.groupBy("polarity") \
+        .count() \
+        .orderBy(col("count").desc()) \
+        .show()
+
+df.groupBy("text") \
+        .count() \
+        .orderBy(col("count").desc()) \
+        .show()
 
 # set seed for reproducibility
-(trainingData, testData) = data.randomSplit([0.7, 0.3], seed=100)
-print("Training Dataset Count: " + str(trainingData.count()))
-print("Test Dataset Count: " + str(testData.count()))
+(training_data, test_data) = df.randomSplit([0.7, 0.3], seed=100)
+print("Training data count: " + str(training_data.count()))
+print("Test data count: " + str(test_data.count()))
 
 # regular expression tokenizer
-regexTokenizer = RegexTokenizer(inputCol="SentimentText", outputCol="words", pattern="\\W")
+regexTokenizer = RegexTokenizer(inputCol="text", outputCol="words", pattern="\\W")
 
 # stop words
-add_stopwords = ["http","https","amp","rt","t","c","the"]
+add_stopwords = ["http", "https", "amp", "rt", "t", "c", "the", "RT", "@"]
 stopwordsRemover = StopWordsRemover(inputCol="words", outputCol="filtered").setStopWords(add_stopwords)
 
 # bag of words count
 countVectors = CountVectorizer(inputCol="filtered", outputCol="features", vocabSize=10000, minDF=5)
 
 # convert string labels to indexes
-label_stringIdx = StringIndexer(inputCol="Sentiment", outputCol="label")
+indexer = StringIndexer(inputCol="polarity", outputCol="label")
 
+# feature-selector
+selector = ChiSqSelector(numTopFeatures=10, featuresCol="features",
+                         outputCol="selectedFeatures", labelCol="label")
+
+# logistic regression model
 lr = LogisticRegression(maxIter=20, regParam=0.3, elasticNetParam=0)
-#lrModel = lr.fit(trainingData)
-
 
 # build the pipeline
-pipeline = Pipeline(stages=[regexTokenizer, stopwordsRemover, countVectors, label_stringIdx, lr])
+pipeline = Pipeline(stages=[regexTokenizer, stopwordsRemover, countVectors, indexer, selector, lr])
 
 # Fit the pipeline to training documents.
-pipelineFit = pipeline.fit(trainingData)
-predictions = pipelineFit.transform(testData)
+pipelineFit = pipeline.fit(training_data)
+prediction = pipelineFit.transform(test_data)
 
-predictions.filter(predictions['prediction'] == 0) \
-    .select("SentimentText","Sentiment","probability","label","prediction") \
-    .orderBy("probability", ascending=False) \
-    .show(n = 10, truncate = 30)
-
-predictions.filter(predictions['prediction'] == 1) \
-    .select("SentimentText","Sentiment","probability","label","prediction") \
-    .orderBy("probability", ascending=False) \
-    .show(n = 10, truncate = 30)
-
-# Evaluate, metricName=[accuracy | f1]default f1 measure
-evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction",labelCol="label")
-print("F1: %g" % (evaluator.evaluate(predictions)))
+# Evaluation
+evaluator = MulticlassClassificationEvaluator()
+print("F1: %g" % (evaluator.evaluate(prediction)))
 
 # save the trained model for future use
-pipelineFit.save("logreg.model")
-
-# PipelineModel.load("logreg.model")
+pipelineFit.write().overwrite().save("logreg.model")
---
+++ a3/spark_streaming.py
@@ -0,0 +1,73 @@
+import sys
+import findspark
+findspark.init()
+
+from pyspark.sql.functions import col
+from pyspark.sql import SQLContext, SparkSession
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from collections import namedtuple
+from pyspark.sql.functions import desc
+from pyspark.ml import PipelineModel
+
+sc = SparkContext("local[2]", "Streaming App")
+
+ssc = StreamingContext(sc, 10)
+sqlContext = SQLContext(sc)
+
+# host = "172.31.38.165"
+# port = 5555
+host = "localhost"
+port = 9999
+
+# Internal ip of  the tweepy streamer
+socket_stream = ssc.socketTextStream(host, port)
+
+lines = socket_stream.window(20)
+lines.pprint()
+fields = ("text")
+Tweet = namedtuple('Tweet', fields)
+
+pipeline_model = PipelineModel.load("logreg.model")
+
+
+def getSparkSessionInstance(sparkConf):
+    if ("sparkSessionSingletonInstance" not in globals()):
+        globals()["sparkSessionSingletonInstance"] = SparkSession \
+            .builder \
+            .config(conf=sparkConf) \
+            .getOrCreate()
+    return globals()["sparkSessionSingletonInstance"]
+
+
+def do_something(time, rdd):
+    print("========= %s =========" % str(time))
+    try:
+        # Get the singleton instance of SparkSession
+        spark = getSparkSessionInstance(rdd.context.getConf())
+
+        # Convert RDD[String] to RDD[Tweet] to DataFrame
+        rowRdd = rdd.map(lambda w: Tweet(w))
+        linesDataFrame = spark.createDataFrame(rowRdd)
+
+        # Creates a temporary view using the DataFrame
+        linesDataFrame.createOrReplaceTempView("tweets")
+
+        # Do tweet character count on table using SQL and print it
+        lineCountsDataFrame = spark.sql("SELECT text FROM tweets")
+
+        prediction = pipeline_model.transform(lineCountsDataFrame)
+        prediction.show()
+
+        prediction.createOrReplaceTempView("tweets_prediction")
+        finalDataFrame = spark.sql("SELECT prediction, text FROM tweets_prediction")
+        finalDataFrame.coalesce(1).write.save(path='csv', format='csv', mode='append')
+    except:
+        sys.exit(0)
+
+
+# key part!
+lines.foreachRDD(do_something)
+
+ssc.start()
+ssc.awaitTermination()
